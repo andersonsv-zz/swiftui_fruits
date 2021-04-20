936ebc0b46f7686725c31b09e1899cd6f817d9f3
Anderson Vieira <anderson.vieira@unidas.com.br>
2021-04-20 07:58:44 -0300

    First commit

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,67 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Anderson Simões Vieira on 19/04/21.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    // MARK: - PROPERTIES
+    var fruit: Fruit
+    
+    //MARK: - BODY
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20){
+                    //HEADER
+                    FruitHeaderView(fruit: fruit)
+                    VStack(alignment: .leading, spacing: 20) {
+                        //TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+
+                        //HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        //NUTRIENTS
+                        
+                        
+                        //SUBHEADLINE
+                        Text("Lear more about \(fruit.title)".uppercased())
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        //LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    }//: VSTACH
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                }
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            }
+            .edgesIgnoringSafeArea(.top)
+        } // :NAVIGATION
+        
+    }
+}
+
+// MARK: - PREVIEW
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+    }
+}
